a04d343f2307991a23b2eaa9be9465b14f185a03
Fastdrecad <andrijas.micun@gmail.com>
2024-07-30 22:05:30 +0200

    Fix custom button component

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -1,4 +1,20 @@
-export default function Button({ text, color, background, icon, disabled }) {
+import React from "react";
+
+interface ButtonProps {
+  text: string;
+  color: string;
+  background: string;
+  icon: string;
+  disabled: boolean;
+}
+
+export default function Button({
+  text,
+  color,
+  background,
+  icon,
+  disabled
+}: ButtonProps) {
   return (
     <button
       className={`bg-indigo-950 px-8 py-4 text-${color} rounded-full font-medium tracking-wider hover:bg-indigo-900`}
