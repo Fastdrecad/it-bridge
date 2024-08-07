e51c8299f0dda4652d17be60c487cffa64f0f2b7
Fastdrecad <andrijas.micun@gmail.com>
2024-08-07 09:01:53 +0200

    Fix google map component

--- app/_components/GoogleMap.tsx
+++ app/_components/GoogleMap.tsx
@@ -1,10 +1,7 @@
+"use client";
+
 import React, { useCallback, useState } from "react";
-import {
-  GoogleMap,
-  Marker,
-  InfoWindow,
-  useLoadScript
-} from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const containerStyle = {
   width: "100%",
@@ -20,11 +17,7 @@ interface MapProps {
   apiKey: string;
 }
 
-const Map: React.FC<MapProps> = ({ apiKey }) => {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: apiKey
-  });
-
+const GoogleMapComponent = () => {
   const [selectedPlace, setSelectedPlace] = useState<{
     lat: number;
     lng: number;
@@ -38,28 +31,63 @@ const Map: React.FC<MapProps> = ({ apiKey }) => {
     setSelectedPlace(null);
   }, []);
 
-  if (loadError) return <div>Error loading maps</div>;
-  if (!isLoaded) return <div>Loading...</div>;
+  const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "";
+
   return (
-    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
-      <Marker
-        position={center}
-        title="Obilićev Venac, Belgrade, Serbia"
-        onClick={handleMarkerClick}
-      />
-      {selectedPlace && (
-        <InfoWindow
-          position={selectedPlace}
-          onCloseClick={handleInfoWindowCloseClick}
-        >
-          <div>
-            <h2>Obilićev Venac</h2>
-            <p>Belgrade, Serbia</p>
-          </div>
-        </InfoWindow>
-      )}
-    </GoogleMap>
+    <LoadScript googleMapsApiKey={googleMapsApiKey}>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+        <Marker
+          position={center}
+          title="Obilićev Venac, Belgrade, Serbia"
+          onClick={handleMarkerClick}
+        />
+      </GoogleMap>
+    </LoadScript>
   );
 };
 
-export default Map;
+export default GoogleMapComponent;
+
+// const Map: React.FC<MapProps> = ({ apiKey }) => {
+//   const { isLoaded, loadError } = useLoadScript({
+//     googleMapsApiKey: apiKey
+//   });
+
+// const [selectedPlace, setSelectedPlace] = useState<{
+//   lat: number;
+//   lng: number;
+// } | null>(null);
+
+// const handleMarkerClick = useCallback(() => {
+//   setSelectedPlace(center);
+// }, []);
+
+// const handleInfoWindowCloseClick = useCallback(() => {
+//   setSelectedPlace(null);
+// }, []);
+
+// if (loadError) return <div>Error loading maps</div>;
+// if (!isLoaded) return <div>Loading...</div>;
+//   return (
+//     <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+//       <Marker
+//         position={center}
+//         title="Obilićev Venac, Belgrade, Serbia"
+//         onClick={handleMarkerClick}
+//       />
+//       {selectedPlace && (
+//         <InfoWindow
+//           position={selectedPlace}
+//           onCloseClick={handleInfoWindowCloseClick}
+//         >
+//           <div>
+//             <h2>Obilićev Venac</h2>
+//             <p>Belgrade, Serbia</p>
+//           </div>
+//         </InfoWindow>
+//       )}
+//     </GoogleMap>
+//   );
+// };
+
+// export default Map;
