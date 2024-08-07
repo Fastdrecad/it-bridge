dbce415e0e9c687e8c03a56b47069db3b6130040
Fastdrecad <andrijas.micun@gmail.com>
2024-08-07 09:05:23 +0200

    Fix google map component bug

--- app/_components/GoogleMap.tsx
+++ app/_components/GoogleMap.tsx
@@ -13,10 +13,6 @@ const center = {
   lng: 20.4571 // Longitude of Obilićev Venac
 };
 
-interface MapProps {
-  apiKey: string;
-}
-
 const GoogleMapComponent = () => {
   const [selectedPlace, setSelectedPlace] = useState<{
     lat: number;
@@ -47,47 +43,3 @@ const GoogleMapComponent = () => {
 };
 
 export default GoogleMapComponent;
-
-// const Map: React.FC<MapProps> = ({ apiKey }) => {
-//   const { isLoaded, loadError } = useLoadScript({
-//     googleMapsApiKey: apiKey
-//   });
-
-// const [selectedPlace, setSelectedPlace] = useState<{
-//   lat: number;
-//   lng: number;
-// } | null>(null);
-
-// const handleMarkerClick = useCallback(() => {
-//   setSelectedPlace(center);
-// }, []);
-
-// const handleInfoWindowCloseClick = useCallback(() => {
-//   setSelectedPlace(null);
-// }, []);
-
-// if (loadError) return <div>Error loading maps</div>;
-// if (!isLoaded) return <div>Loading...</div>;
-//   return (
-//     <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
-//       <Marker
-//         position={center}
-//         title="Obilićev Venac, Belgrade, Serbia"
-//         onClick={handleMarkerClick}
-//       />
-//       {selectedPlace && (
-//         <InfoWindow
-//           position={selectedPlace}
-//           onCloseClick={handleInfoWindowCloseClick}
-//         >
-//           <div>
-//             <h2>Obilićev Venac</h2>
-//             <p>Belgrade, Serbia</p>
-//           </div>
-//         </InfoWindow>
-//       )}
-//     </GoogleMap>
-//   );
-// };
-
-// export default Map;
